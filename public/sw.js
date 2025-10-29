4d2c8c6058709554934e0a8354dcd494ec724a50
darumcat <alexs14@list.ru>
2025-10-29 15:57:58 +1100

    change to pwa

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'onepunchconvert-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.tsx',
+  '/manifest.json',
+  '/assets/saitama-icon-192.png',
+  '/assets/saitama-icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
